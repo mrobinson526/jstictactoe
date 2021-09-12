c4872f3774bb97794e803b49be6c40f6f3f9fdab
Margaret <mrobinson526@gmail.com>
2021-09-12 18:12:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+let players = ["X", "O"];
+let cells = document.querySelectorAll(".row>div");
+for (let i=0; i<cells.length; i++) {
+    cells[i].addEventListener("click", cellClicked);
+}
+function cellClicked() {
+        event.target.textContent="X";
+};
+function checkWin(){
+    if(cells[0].textContent ==="X" 
+      && cells[1].textContent ==="X" 
+      && cells[2].textContent ==="X") 
+    {alert("Player X is the winner")} 
+    else if (cells[3].textContent ==="X"
+      && cells[4].textContent ==="X"
+      && cells[5].textContent ==="X")
+    {alert("Player X is the winner")} 
+    else if (cells[6].textContent ==="X"
+      && cells[7].textContent ==="X"
+      && cells[8].textContent ==="X")
+    {alert("Player X is the winner")}
+    else if (cells[0].textContent ==="X"
+      && cells[3].textContent ==="X"
+      && cells[6].textContent ==="X")
+    {alert("Player X is the winner")}
+    else if (cells[1].textContent ==="X"
+      && cells[4].textContent ==="X"
+      && cells[7].textContent ==="X")
+    {alert("Player X is the winner")}
+    else if (cells[2].textContent ==="X"
+      && cells[5].textContent ==="X"
+      && cells[8].textContent ==="X")
+    {alert("Player X is the winner")}  
+    else if (cells[0].textContent ==="X"
+      && cells[4].textContent ==="X"
+      && cells[8].textContent ==="X")
+    {alert("Player X is the winner")}
+    else if (cells[2].textContent ==="X"
+      && cells[4].textContent ==="X"
+      && cells[6].textContent ==="X")
+    {alert("Player X is the winner")}
+
+    if(cells[0].textContent ==="O" 
+      && cells[1].textContent ==="O" 
+      && cells[2].textContent ==="0") 
+    {alert("Player O is the winner")} 
+    else if (cells[3].textContent ==="O"
+      && cells[4].textContent ==="O"
+      && cells[5].textContent ==="O")
+    {alert("Player O is the winner")} 
+    else if (cells[6].textContent ==="O"
+      && cells[7].textContent ==="O"
+      && cells[8].textContent ==="O")
+    {alert("Player O is the winner")}
+    else if (cells[0].textContent ==="O"
+      && cells[3].textContent ==="O"
+      && cells[6].textContent ==="O")
+    {alert("Player O is the winner")}
+    else if (cells[1].textContent ==="O"
+      && cells[4].textContent ==="O"
+      && cells[7].textContent ==="O")
+    {alert("Player O is the winner")}
+    else if (cells[2].textContent ==="O"
+      && cells[5].textContent ==="O"
+      && cells[8].textContent ==="O")
+    {alert("Player O is the winner")}  
+    else if (cells[0].textContent ==="O"
+      && cells[4].textContent ==="O"
+      && cells[8].textContent ==="O")
+    {alert("Player O is the winner")}
+    else if (cells[2].textContent ==="O"
+      && cells[4].textContent ==="O"
+      && cells[6].textContent ==="O")
+    {alert("Player O is the winner")}    
+} 
+  
